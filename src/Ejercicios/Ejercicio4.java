e41d2c3b20f77b7c8da07bffaa0a4b950070da3a
Anaíza Araiza <a281949@uach.mx>
2016-09-04 18:32:32 -0600

    Se realiza ejercicio 4

---
+++ src/Ejercicios/Ejercicio4.java
@@ -0,0 +1,15 @@
+package Ejercicios;
+
+import java.util.stream.IntStream;
+
+/**
+ * @author iza19
+ * @version 1.0
+ *
+ * Hacer un pseudocódigo que imprima la suma de los 100 primeros números.
+ */
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        System.out.println(IntStream.rangeClosed(1, 100).sum());
+    }
+}
