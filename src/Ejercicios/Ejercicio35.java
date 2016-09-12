7e1e0192ed63be37cb9939c2d6b837b3461144c4
Anaíza Araiza <a281949@uach.mx>
2016-09-12 11:16:26 -0600

    Se resuelve ejercicio 35

--- src/Ejercicios/Ejercicio35.java
+++ src/Ejercicios/Ejercicio35.java
@@ -1,5 +1,7 @@
 package Ejercicios;
 
+import java.util.Vector;
+
 /**
  * @author iza19
  * @version 1.0
@@ -8,5 +10,17 @@ package Ejercicios;
  */
 public class Ejercicio35 {
     public static void main(String[] args) {
+        Vector<Double> calificaciones = new Vector<>(10);
+        calificaciones.addElement(6.0);
+        calificaciones.addElement(7.8);
+        calificaciones.addElement(9.0);
+        calificaciones.addElement(8.8);
+        calificaciones.addElement(8.0);
+        calificaciones.addElement(9.8);
+        calificaciones.addElement(4.0);
+        calificaciones.addElement(6.8);
+        calificaciones.addElement(9.0);
+        calificaciones.addElement(10.0);
+        System.out.println(calificaciones.stream().mapToDouble(Double::doubleValue).findAny().getAsDouble());
     }
 }
