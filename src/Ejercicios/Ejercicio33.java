16028fb59c72e59e96c14b5d042f8a12503a6f40
Anaíza Araiza <a281949@uach.mx>
2016-09-12 09:46:21 -0600

    Se resuelve ejercicio 33

--- src/Ejercicios/Ejercicio33.java
+++ src/Ejercicios/Ejercicio33.java
@@ -1,5 +1,8 @@
 package Ejercicios;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author iza19
  * @version 1.0
@@ -9,5 +12,28 @@ package Ejercicios;
  */
 public class Ejercicio33 {
     public static void main(String[] args) {
+        List<String> personas = new ArrayList<>();
+        personas.add("Luis");
+        personas.add("Óscar");
+        personas.add("Jorge");
+        personas.add("Antonio");
+        personas.add("Armando");
+        personas.add("Arturo");
+        personas.add("Cristian");
+        personas.add("Karina");
+        personas.add("Manuel");
+        personas.add("Omar");
+        personas.add("Alejandro");
+        personas.add("Sorely");
+        personas.add("Estafanía");
+        personas.add("Marco");
+        personas.add("Enrique");
+        personas.add("Edgar");
+        personas.add("Hiram");
+        personas.add("Raúl");
+        personas.add("Isaac");
+        personas.add("César");
+
+        personas.forEach(System.out::println);
     }
 }
