babb163e38dbadda2f4cf144ab40b467b4079d5e
Anaíza Araiza <a281949@uach.mx>
2016-09-12 11:12:26 -0600

    Se resuelve ejercicio 34

--- src/Ejercicios/Ejercicio34.java
+++ src/Ejercicios/Ejercicio34.java
@@ -1,5 +1,7 @@
 package Ejercicios;
 
+import java.util.Vector;
+
 /**
  * @author iza19
  * @version 1.0
@@ -9,5 +11,17 @@ package Ejercicios;
  */
 public class Ejercicio34 {
     public static void main(String[] args) {
+        Vector<Double> calificaciones = new Vector<>(10);
+        calificaciones.addElement(6.0);
+        calificaciones.addElement(7.8);
+        calificaciones.addElement(9.0);
+        calificaciones.addElement(8.8);
+        calificaciones.addElement(8.0);
+        calificaciones.addElement(9.8);
+        calificaciones.addElement(4.0);
+        calificaciones.addElement(6.8);
+        calificaciones.addElement(9.0);
+        calificaciones.addElement(10.0);
+        System.out.println((calificaciones.stream().mapToDouble(Double::doubleValue).sum()) / 10);
     }
 }
