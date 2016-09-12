eead7cc1a011f06005d51ccd87ffff8f3a16c79b
Anaíza Araiza <a281949@uach.mx>
2016-09-12 11:43:26 -0600

    Se resuelve ejercicio 36

--- src/Ejercicios/Ejercicio36.java
+++ src/Ejercicios/Ejercicio36.java
@@ -1,5 +1,8 @@
 package Ejercicios;
 
+import java.util.Random;
+import java.util.stream.IntStream;
+
 /**
  * @author iza19
  * @version 1.0
@@ -8,5 +11,16 @@ package Ejercicios;
  */
 public class Ejercicio36 {
     public static void main(String[] args) {
+        IntStream row1 = new Random().ints(5, 1, 100);
+        IntStream row2 = new Random().ints(5, 1, 100);
+        IntStream row3 = new Random().ints(5, 1, 100);
+        IntStream row4 = new Random().ints(5, 1, 100);
+        row1.forEach((n) -> System.out.print(n + "   "));
+        System.out.println();
+        row2.forEach((n) -> System.out.print(n + "   "));
+        System.out.println();
+        row3.forEach((n) -> System.out.print(n + "   "));
+        System.out.println();
+        row4.forEach((n) -> System.out.print(n + "   "));
     }
 }
